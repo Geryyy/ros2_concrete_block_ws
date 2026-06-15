b72b8ba1954696560168eec257732fda4f37e098
Gerald Ebmer <gerald.ebmer@gmail.com>
2026-06-15 13:25:12 +0000

    Add perception debug captures

--- play_bag.sh
+++ play_bag.sh
@@ -4,10 +4,10 @@ set -e
 
 # BAG_DIR="/home/vscode/Documents/pzs_crane_1_pickup"
 # BAG_DIR="/home/vscode/Documents/pzs_crane_2_placement_rotated"
-BAG_DIR="/home/vscode/Documents/pzs_crane_3_pickup_rotated"
+# BAG_DIR="/home/vscode/Documents/pzs_crane_3_pickup_rotated"
 # BAG_DIR="/home/vscode/Documents/pzs_crane_4_placement_far"
 # BAG_DIR="/home/vscode/Documents/pzs_crane_5_stacking"
-# BAG_DIR="/home/vscode/Documents/pzs_crane_6_stacking_rotated"
+BAG_DIR="/home/vscode/Documents/pzs_crane_6_stacking_rotated"
 # BAG_DIR="/home/vscode/Documents/pzs_crane_7_palettes"
 # BAG_DIR="/home/vscode/Documents/pzs_crane_8_palettes"
 RATE="${1:-1.0}"
